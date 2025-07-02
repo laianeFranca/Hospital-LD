dd6a8abac2627f705da8904f9eeee9109e3f0d82
laianeFranca <vlaiane940@gmail.com>
2025-07-02 09:43:45 -0300

    Create data.sql

---
+++ app/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO enfermeiros (nome, cpf, turno) 
+VALUES 
+('Ana Souza', '123.456.789-00', 'Manhã'),
+('Carlos Lima', '987.654.321-00', 'Tarde'),
+('Juliana Silva', '456.789.123-00', 'Noite');
